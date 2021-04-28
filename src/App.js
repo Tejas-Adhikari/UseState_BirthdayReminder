f35a8cfa758a0d848c23697dbd11cc4e4884048b
Tejas-Adhikari <tejas.adhikari@somaiya.edu>
2021-04-28 11:18:05 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
+function App() {
+  const [people, setPeople] = useState(data);
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
+  return (
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <>
+          {people.map((person) => {
+            const { id, name, age, image } = person;
+            return (
+              <article key={id} className="person">
+                <img src={image} alt={name} />
+                <div>
+                  <h4>{name}</h4>
+                  <p>{age} years</p>
+                  <button onClick={() => removeItem(id)}>Remove</button>
+                </div>
+              </article>
+            );
+          })}
+        </>
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: "Tejas Adhikari",
+    age: 22,
+    image:
+      "https://res.cloudinary.com/tejas2298/image/upload/v1619363156/react/birthday-reminder/IMG-20210403-WA0009_xzy0z2.jpg",
+  },
+  {
+    id: 2,
+    name: "Hester Hogan",
+    age: 32,
+    image:
+      "https://res.cloudinary.com/tejas2298/image/upload/v1619361489/samples/people/smiling-man.jpg",
+  },
+  {
+    id: 3,
+    name: "Larry Little",
+    age: 36,
+    image:
+      "https://res.cloudinary.com/tejas2298/image/upload/v1619361491/samples/people/boy-snow-hoodie.jpg",
+  },
+  {
+    id: 4,
+    name: "Sean Walsh",
+    age: 34,
+    image:
+      "https://res.cloudinary.com/tejas2298/image/upload/v1619361491/samples/people/jazz.jpg",
+  },
+  {
+    id: 5,
+    name: "Lola Gardner",
+    age: 29,
+    image:
+      "https://res.cloudinary.com/tejas2298/image/upload/v1619361492/samples/people/bicycle.jpg",
+  },
+];
